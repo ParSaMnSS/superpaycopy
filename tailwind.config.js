2bda41036b554a7632502371917c6a15b5467aad
MNS <parsamanes@yahoo.com>
2024-08-23 14:32:12 +0300

    god help me

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
   content: ["./templates/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': '#1692AC',
+        'custom-pink': '#AA1A55',
+        'custom-red': '#F7435F',
+        'custom-orange': '#F78160',
+      },
       minHeight: {
         '95': '23.3rem', // 372.8px
       },

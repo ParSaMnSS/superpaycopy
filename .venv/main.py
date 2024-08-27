473d77256f73c02eb7f586553664bf9787898d0b
MNS <parsamanes@yahoo.com>
2024-08-27 12:34:25 +0300

    saving before adding responsive ting

--- .venv/main.py
+++ .venv/main.py
@@ -8,6 +8,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
